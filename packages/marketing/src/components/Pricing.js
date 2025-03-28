a737eda0aa4721a2d7ab814efcbbaf3252f93947
SamyuMT <jemycraft12@hotmail.com>
2025-03-28 12:33:25 -0500

    prueba 1

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -161,7 +161,7 @@ export default function Pricing() {
           color="textSecondary"
           component="p"
         >
-          Quickly build an effective pricing table a for your potential customers
+          Quickly build a effective pricing table a for your potential customers
           with this layout. It&apos;s built with default Material-UI components
           with little customization.
         </Typography>
